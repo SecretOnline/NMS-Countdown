dc8b78ea3c46e37805302288555908dc30f82474
secret_online <SecretOnline@users.noreply.github.com>
2015-07-16 20:30:52 +1200

    Add background image fading
    Man, this took a while to get right.
    
    Works nicely.
    
    Also fixed missing </main>

--- countdown.js
+++ countdown.js
@@ -18,6 +18,47 @@ function initPage() {
 
   var ircButton = $('.ircButton');
   ircButton.addEventListener('click', toggleIrc);
+
+  setInterval(changeImage, 30000);
+  changeImage();
+}
+
+function changeImage() {
+  var $ = document.querySelector.bind(document);
+  var bgElement = $('.bg');
+  var body = $('body');
+  bgElement.classList.remove('fadeout');
+  bgElement.style.opacity = 1; // Manually specify opacity
+  bgElement.style.backgroundImage = body.style.backgroundImage;
+  setTimeout(function() {
+    bgElement.classList.add('fadeout');
+    bgElement.style.opacity = ''; // Remove overly-specific rule to allow animation to work
+  }, 1000);
+
+  body.style.backgroundImage = 'url(img/' + nextImage() + ')';
+
+}
+
+function nextImage() {
+  return data_images[Math.floor(Math.random() * data_images.length)];
 }
 
 window.addEventListener('load', initPage);
+
+// Long list of images
+// 'img/' gets added on automatically
+var data_images = [
+  'AlpineFly.png',
+  'Becron5.png',
+  'Creature.png',
+  'Diplo.png',
+  'Fleet.png',
+  'GlattrecSystem.png',
+  'NewEridu.png',
+  'NoMansSky_GalacticMap.png',
+  'NoMansSky_Monolith.png',
+  'NoMansSky_SpaceStationTube.png',
+  'station1.jpg',
+  'station2.jpg',
+  'station3.jpg'
+];
